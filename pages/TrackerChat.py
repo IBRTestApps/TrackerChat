2b0e86294ea53184d95ffeddb5c8f400879e543c
Rick Struiksma <rick.struiksma@gmail.com>
2024-09-07 16:16:11 +0200

    Agent REACT prompt aangepast

--- pages/TrackerChat.py
+++ pages/TrackerChat.py
@@ -50,7 +50,7 @@ Use the following format:
 Question: the input question you must answer
 Thought: you should always think about what to do
 Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action. You should NOT rewrite the user's question.
+Action Input: the input to the action. You MUST use the user's input question.
 Observation: the result of the action
 ... (this Thought/Action/Action Input/Observation can repeat N times)
 Thought: I now know the final answer
